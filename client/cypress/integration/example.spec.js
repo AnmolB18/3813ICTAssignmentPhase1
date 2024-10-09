092478061fbc587c1ac8b8deeface1696cacf64c
AnmolB18 <molanmol11@gmail.com>
2024-10-09 16:13:47 +1000

    e2e Initial Commit

---
+++ client/cypress/integration/example.spec.js
@@ -0,0 +1,8 @@
+// cypress/integration/example.spec.js
+
+describe('My First Test', () => {
+    it('Visits the app and checks the title', () => {
+        cy.visit('http://localhost:4200'); // Change this to your app's URL
+        cy.title().should('include', 'Your App Title'); // Adjust to your app's title
+    });
+});
